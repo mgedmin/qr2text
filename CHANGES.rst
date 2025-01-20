d281af323d3d93dd2da09f64338db9206c583d42
Marius Gedminas <marius@gedmin.as>
2025-01-20 14:17:29 +0200

    Drop Python 3.7, GHA no longer supports it

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,7 +4,7 @@ Changelog
 1.1.2 (unreleased)
 ------------------
 
-- Nothing changed yet.
+- Drop support for Python 3.7.
 
 
 1.1.1 (2024-10-09)
--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -45,7 +44,7 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     license="GPL",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 
     py_modules=["qr2text"],
     zip_safe=False,
